9b145b76b49194d6831095b5ad827cc8e6aa5271
mrtobititiloye2748 <mrtobititiloye@gmail.com>
2026-04-04 18:56:33 -0400

    Version 2 Step 1: Added Passenger struct to pricing.h for full booking simulation

--- DynamicTicketPricingProject#Version1/pricing.h
+++ DynamicTicketPricingProject#Version1/pricing.h
@@ -1,6 +1,21 @@
 #ifndef PRICING_H
 #define PRICING_H
 
+typedef struct {
+    int isWeekend;
+    int isTouristDestination;
+    int hasLoyaltyProgram;
+
+    int wantsWindow;
+    int wantsFront;
+    int wantsAisle;
+
+    double totalWeight;
+    int totalBags;
+
+    int daysBeforeTrip;
+} Passenger;
+
 double calculate_ticket_price(int isWeekend, int isTouristDestination, int hasLoyaltyProgram);
 double calculate_refund(int daysBeforeTrip, int hasLoyaltyProgram);
 double online_checkin(double totalWeight, int totalBags);
